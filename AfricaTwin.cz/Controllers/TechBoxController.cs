ae3b30b6bb8c7dbd5616e34bf4dfb58001428d74
lukasxrv <lukasxrv@gmail.com>
2014-02-01 19:11:16 +0100

    Doplněn entity model a první test čtení z DB

--- AfricaTwin.cz/Controllers/TechBoxController.cs
+++ AfricaTwin.cz/Controllers/TechBoxController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AfricaTwin.cz.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,12 @@ namespace AfricaTwin.cz.Controllers
         // GET: /TechBox/
         public ActionResult TechBox()
         {
+            www_africatwin_czEntities atDB = new www_africatwin_czEntities();
+
+            int pocetZaznamuTechBoxu = atDB.at_polozky.Where(p => p.kategorie.StartsWith("techbox")).Count();
+
+            ViewBag.PocetPolozek = pocetZaznamuTechBoxu;
+            
             return View();
         }
 	}
